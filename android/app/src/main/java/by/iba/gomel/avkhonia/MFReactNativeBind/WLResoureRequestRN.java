ec65f1f545ecbaa22e49c6f58feedd591168aee0
yavelikiy <yavelikiy@github.com>
2017-04-14 00:38:11 +0300

    Added Android & IOS MF API

---
+++ android/app/src/main/java/by/iba/gomel/avkhonia/MFReactNativeBind/WLResoureRequestRN.java
@@ -0,0 +1,101 @@
+/**
+ *    © Copyright 2016 IBM Corp.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package by.iba.gomel.avkhonia.MFReactNativeBind;
+
+import android.util.Log;
+
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.uimanager.IllegalViewOperationException;
+import com.worklight.wlclient.api.WLFailResponse;
+import com.worklight.wlclient.api.WLResourceRequest;
+import com.worklight.wlclient.api.WLResponse;
+import com.worklight.wlclient.api.WLResponseListener;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by ishaib on 15/09/16.
+ */
+public class WLResourceRequestRN extends ReactContextBaseJavaModule {
+    public WLResourceRequestRN(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "WLResourceRequestRN";
+    }
+
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put("GET", WLResourceRequest.GET);
+        constants.put("POST", WLResourceRequest.POST);
+        return constants;
+    }
+
+    @ReactMethod
+    public void requestWithURL(
+            String url,
+            String method,
+            final Callback errorCallback,
+            final Callback successCallback) {
+        try {
+            WLResourceRequest request = new WLResourceRequest(URI.create(url),method);
+            request.send(new WLResponseListener(){
+                public void onSuccess(WLResponse response) {
+                    successCallback.invoke(response.getResponseText());
+                    Log.d("Success", response.getResponseText());
+                }
+                public void onFailure(WLFailResponse response) {
+                    errorCallback.invoke(response.getErrorMsg());
+                    Log.d("Failure", response.getErrorMsg());
+                }
+            });
+        } catch (IllegalViewOperationException e) {
+            errorCallback.invoke(e.getMessage());
+        }
+    }
+
+    @ReactMethod
+    public void asyncRequestWithURL(
+            String url,
+            String method,
+            final Promise promise) {
+        try {
+            WLResourceRequest request = new WLResourceRequest(URI.create(url),method);
+            request.send(new WLResponseListener(){
+                public void onSuccess(WLResponse response) {
+                    promise.resolve(response.getResponseText());
+                    Log.d("Success", response.getResponseText());
+                }
+                public void onFailure(WLFailResponse response) {
+                    promise.reject(response.getErrorStatusCode(), response.getErrorMsg());
+                    Log.d("Failure", response.getErrorMsg());
+                }
+            });
+        } catch (IllegalViewOperationException e) {
+            promise.reject("failure" ,e.getMessage(), e);
+        }
+    }
+}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,10 +13,70 @@ import Container from '../components/Container';
 import Button from '../components/Button';
 import Label from '../components/Label';
 
-export default class Login extends Component {
+var WLResourceRequestRN = require('NativeModules').WLResourceRequestRN;
 
-  press() {
-  	Alert.alert('Button has been pressed!');
+export default class Login extends Component {
+	constructor(props) {
+	        super(props);
+	        this.state = {
+	            isLoading: false,
+	            message: ''
+	        };   
+	        //this.registerChallengeHandler();
+	    }
+
+    async getMFBlogEnriesAsPromise() {
+        //SecurityCheckChallengeHandlerRN.cancel("UserLogin");
+        var error = "";
+        this.setState({ isLoading: true, message: '' });
+        try {
+            var result
+            result = await WLResourceRequestRN.asyncRequestWithURL("/adapters/MFBlogAdapter/getFeed", WLResourceRequestRN.GET);
+            this.handleResponse(JSON.parse(result))
+        } catch (e) {
+            error = e;
+        }
+        this.setState({ isLoading: false, message: error ? "Failed to retrieve blog entries - " + error.message : ""});
+    }
+
+    getMFBlogEnriesAsCallback() {
+        //SecurityCheckChallengeHandlerRN.cancel("UserLogin");
+        var that = this;
+        this.setState({ isLoading: true, message: '' });
+        WLResourceRequestRN.requestWithURL("/adapters/MFBlogAdapter/getFeed", WLResourceRequestRN.GET,
+            (error) => {
+                //that.props.navigator.popToTop();
+                that.setState({ isLoading: false, message: error.message });
+            },
+            (result) => {
+                that.handleResponse(JSON.parse(result))
+                that.setState({ isLoading: false, message: "" });
+            });
+    }	
+
+    handleResponse(response) {
+        /*this.setState({ isLoading: false, message: '' });
+        var beComponent = {
+            title: 'MF And ReactNative Demo',
+            component: BlogEntries,
+            passProps: { entries: response.feed.entry }
+        };
+
+        if (this.isLoginOnTop()) {
+            this.props.navigator.replace(beComponent);
+        } else {
+            this.props.navigator.push(beComponent);
+        }*/
+        
+    }
+
+  pressForget() {
+  	Alert.alert('Go to admins, please :)');
+  }
+
+  pressSignIn() {
+  	Alert.alert('Navigate to sign in form..');
+  	getMFBlogEnriesAsCallback();
   }
 
   render() {
@@ -26,7 +86,7 @@ export default class Login extends Component {
 			    <Button 
 			        label="Forgot Login/Pass"
 			        styles={{button: styles.alignRight, label: styles.label}} 
-			        onPress={this.press.bind(this)} />
+			        onPress={this.pressForget.bind(this)} />
 			</Container>
 			<Container>
 			    <Label text="Username or Email" />
